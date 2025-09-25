de0a4a95a3f83301ba5b4674152f6e7e693bcc48
Kenneth Plumstead <kennethplumstead@icloud.com>
2025-09-25 12:22:08 -0400

    Initial Commit

---
+++ TimsRun/TimsRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimsRunApp.swift
+//  TimsRun
+//
+//  Created by Kenneth Plumstead on 2025-09-25.
+//
+
+import SwiftUI
+
+@main
+struct TimsRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
